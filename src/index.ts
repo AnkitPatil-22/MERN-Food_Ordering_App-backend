582c45ac60fd8ff6ef904f530054e3f713616210
Ankit Patil <ankitpatil2202@gmail.com>
2025-11-15 18:16:43 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import type { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
+
+mongoose
+    .connect(process.env.MONGODB_CONNECTION_STRING as string)
+    .then(() => console.log("Connected to database!"));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/health", (req: Request, res: Response) => {
+    res.send({ message: "health OK!" });
+});
+
+app.use("/api/my/user", myUserRoute);
+
+// app.get("/test", async (req: Request, res: Response) => {
+//     res.json({ message: "Hello!" });
+// });
+
+app.listen(7000, () => {
+    console.log("🚀 Server running at http://localhost:7000");
+});
