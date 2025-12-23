0afebaab9c202d853c82aaa830a2ab402ed774d0
Ankit Patil <ankitpatil2202@gmail.com>
2025-12-23 16:47:38 +0530

    added restaurant details api

--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -3,6 +3,17 @@ import { param } from "express-validator";
 import RestaurantController from "../controllers/RestaurantController";
 
 const router = express.Router();
+
+router.get(
+    "/:restaurantId",
+    param("city")
+        .isString()
+        .trim()
+        .notEmpty()
+        .withMessage("RestaurantId paramenter must be a valid string"),
+    RestaurantController.getRestaurant
+);
+
 router.get(
     "/search/:city",
     param("city")
