898f9379fd7216b6a84a6ed8e8a4477f7f9e8cee
Ankit Patil <ankitpatil2202@gmail.com>
2025-12-26 14:50:34 +0530

    added checkout flow

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import myUserRoute from "./routes/MyUserRoute";
 import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 
 mongoose
     .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -19,8 +20,12 @@ cloudinary.config({
 });
 
 const app = express();
-app.use(express.json());
+
 app.use(cors());
+
+app.post("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json());
 
 app.get("/health", (req: Request, res: Response) => {
     res.send({ message: "health OK!" });
@@ -29,6 +34,7 @@ app.get("/health", (req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
 
 // app.get("/test", async (req: Request, res: Response) => {
 //     res.json({ message: "Hello!" });
